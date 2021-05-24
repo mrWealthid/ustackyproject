5190596fcfc366b73c8e0e86100dde42b92db7bb
Wealth Iduwe <wealthiduwe@gmail.com>
2021-05-24 02:34:04 +0100

    paystack

--- js/cart.js
+++ js/cart.js
@@ -22,6 +22,7 @@ const syncPrice = () => {
   document.querySelector(
     "#total"
   ).innerHTML = `Total Amount:  <span>&#8358</span>   ${total}`;
+  return total;
 };
 
 //Generating cart markup from local storage-cart
@@ -165,9 +166,87 @@ const errorMsg3 = document.querySelector(".errorMsg3");
 
   tel.addEventListener("blur", () => {
     if (Number.isNaN(parseInt(tel.value)) || tel.value == "") {
-      errorMsg3.innerHTML = "Please enter a valid phone number";
+      errorMsg3.innerHTML = "Input can only be numbers";
+    } else if (tel.value.length < 11) {
+      errorMsg3.innerHTML = "Phone numbers cannot be less than 11 digits";
     } else {
       errorMsg3.innerHTML = "";
     }
   });
 })();
+
+function payWithPaystack() {
+  console.log("test");
+  let handler = PaystackPop.setup({
+    key: "pk_test_c8d49ba5c7751a42402cb85eeb58485e3bb3d0f6", // Replace with your public key
+    email: document.getElementById("email").value,
+    amount: syncPrice() * 100,
+    // amount: syncPrice() * 100,
+    ref: "" + Math.floor(Math.random() * 1000000000 + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
+    // label: "Optional string that replaces customer email"
+    onClose: function () {
+      alert("Window closed.");
+    },
+    callback: function (response) {
+      setTimeout(showSummary, 1000);
+    },
+  });
+  handler.openIframe();
+}
+
+function clearCart() {
+  let cart = getCart();
+  cart.splice(0, cart.length);
+  localStorage.setItem(cartName, JSON.stringify(cart));
+  email.value = "";
+  name.value = "";
+  tel.value = "";
+
+  closeModal();
+  syncPrice();
+  getCartItems();
+}
+
+// Get the modal<script>
+const modal = document.getElementById("myModal");
+
+const showSummary = () => {
+  let cart = getCart();
+
+  modal.style.display = "block";
+  let names = document.getElementById("name").value;
+  document.getElementById(
+    "content-head"
+  ).innerHTML = `<div class= summary-heading><h2>Thanks ${names}, your payment was successful</h2>
+  
+  <img id ='check' src= "images/check.svg"/>
+  </div>`;
+
+  document.getElementById(
+    "content"
+  ).innerHTML = `<div class = 'summary-subheading'>
+
+  <span>S/No</span>
+  <span>Name</span>
+  <span>Quantity</span>
+  </div>`;
+  cart.forEach((item, index) => {
+    document.getElementById(
+      "content"
+    ).innerHTML += `<div class ='summary-items'>
+  <span>${index + 1}</span>
+  <span> ${item.name} </span>
+  <span> ${item.quantity}</span>
+  </div>`;
+  });
+};
+
+const closeModal = () => {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    closeModal();
+  }
+};
